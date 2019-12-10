e8c4ab1b6f179f1828c7e76774c2ecf36bd756fc
Philipp Normann <philipp.normann@gmail.com>
2019-12-10 09:34:38 +0100

    Make main interactive

--- zigbear/main.py
+++ zigbear/main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import serial
 from zigbear.radio import send_packet
@@ -6,12 +7,13 @@ from zigbear.zigbee import parse_frame, bump_counters
 SERIAL_PORT = '/dev/ttyS0'
 BAUD_RATE = 38400
 TIMEOUT = 10
+
 TRANSPOT_KEY = b'\x00\x08\xd0$\xb4\xd4\x08\xfc\xb4\xfcd\x08\xa4\xfc\xbc,'
 
 
 def main():
-    turn_on = False
-    min_fc = 35_750
+    turn_on = True
+    min_fc = 36590
     packet = b"\x41\x88\x4f\x03\xae\xff\xff\x00\x00\x08\x02\xfd\xff\x00\x00\x0a" \
              b"\xd8\x28\xc5\x57\x00\x00\x6a\xfe\x03\xff\xff\x2e\x21\x00\x00\x7c" \
              b"\xbe\x79\x42\x58\xe3\x2a\xf7\xb4\x88\x89\x7b\xc2\x90\x9a\x4d\xac\xbf"
@@ -20,11 +22,10 @@ def main():
     ser.write(b'\n')
 
     while True:
+        cmd = input("ON (1) or OFF (0): ")
+        turn_on = cmd == 'ON' or cmd == '1'
         packet, _fc = bump_counters(packet, TRANSPOT_KEY,  min_fc, turn_on)
-        print(len(packet))
-        print(packet)
         send_packet(ser, packet[:-2])
-        time.sleep(0.1)
 
 
 if __name__ == "__main__":
