229c42181e21ce98c0ec50eca9bb46c379418e40
Philipp Normann <philipp.normann@otto.de>
2019-12-21 17:48:17 +0100

    Fix repo link

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="zigbear",
-    version="0.1.0",
-    description="A curious bear looking for some ZigBee honey 🐻🐝",
-    author="Philipp Normann, Marc Mettke, Torben Tietgen",
-    url="https://github.com/philippnormann1337/ZigBear",
-    install_requires=["pycryptodomex==3.9.4", "pyserial==3.4", "scapy==2.4.3"],
-    packages=["zigbear"],
+    name='zigbear',
+    version='0.1.0',
+    description='A curious bear looking for some ZigBee honey 🐻🐝',
+    author='Philipp Normann, Marc Mettke, Torben Tietgen',
+    url='https://github.com/philippnormann1337/zigbear',
+    install_requires=['pycryptodomex==3.9.4', 'pyserial==3.4', 'scapy==2.4.3'],
+    packages=['zigbear'],
 )
