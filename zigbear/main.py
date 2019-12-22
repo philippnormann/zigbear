728a631ba290b156234da0d1a03ee67fe28cfb94
Philipp Normann <philipp.normann@gmail.com>
2019-12-22 19:29:09 +0100

    Replace hardcoded aps payload bytes for toggle cmd

--- zigbear/main.py
+++ zigbear/main.py
@@ -2,11 +2,7 @@ import sys
 import time
 import serial
 from zigbear.radio.raspbee import send_packet
-from zigbear.packets import parse_frame, bump_counters, encrypted_toggle
-from zigbear.crypto import zigbee_packet_decrypt, zigbee_packet_encrypt
-from zigbear.utils import extended_address_bytes, get_extended_source
-from zigbear.patch.zigbee import ZigbeeNWK
-from scapy.layers.dot15d4 import Dot15d4FCS
+from zigbear.packets import encrypted_toggle
 
 SERIAL_PORT = '/dev/ttyS0'
 BAUD_RATE = 38400
@@ -16,10 +12,16 @@ TRANSPOT_KEY = b'\x00\x08\xd0$\xb4\xd4\x08\xfc\xb4\xfcd\x08\xa4\xfc\xbc,'
 
 
 def main():
-    fc = 38200
-    encrypted_frame = b"\x41\x88\x4f\x03\xae\xff\xff\x00\x00\x08\x02\xfd\xff\x00\x00\x0a" \
-                      b"\xd8\x28\xc5\x57\x00\x00\x6a\xfe\x03\xff\xff\x2e\x21\x00\x00\x7c" \
-                      b"\xbe\x79\x42\x58\xe3\x2a\xf7\xb4\x88\x89\x7b\xc2\x90\x9a\x4d\xac\xbf"
+    panid = 0xae03
+    source = 0x0000
+    destination = 0xffff
+    extended_source = 0x00212effff03fe6a  # Extended Source: Dresden-_ff:ff:03:fe:6a
+
+    frame_counter = 25055
+    seq_num = 0
+    nwk_seq_num = 0
+    aps_counter = 0
+    zcl_seq_num = 0
 
     ser = serial.Serial(port=SERIAL_PORT, baudrate=BAUD_RATE, timeout=TIMEOUT)
     ser.write(b'\n')
@@ -27,21 +29,19 @@ def main():
     while True:
         input("Enter to toggle lamp: ")
 
-        # parsed_frame = parse_frame(encrypted_frame)
+        frame = encrypted_toggle(panid, source, destination, extended_source,
+                                 TRANSPOT_KEY, frame_counter=frame_counter,
+                                 seq_num=seq_num, nwk_seq_num=nwk_seq_num,
+                                 aps_counter=aps_counter, zcl_seq_num=zcl_seq_num).build()
 
-        # extended_source_bytes = extended_address_bytes(get_extended_source(parsed_frame))
-        # decrypted_payload, valid = zigbee_packet_decrypt(TRANSPOT_KEY, parsed_frame, extended_source_bytes)
-        # assert valid, 'Decryption failed!'
+        send_packet(ser, frame[:-2])
+        print(f'Frame counter: {frame_counter}')
 
-        # new_parsed_frame, new_payload = bump_counters(parsed_frame, decrypted_payload, fc)
-        # new_parsed_frame[Dot15d4FCS].fcf_ackreq = 1
-        # new_parsed_frame[ZigbeeNWK].destination = 0x3ef6
-        
-        # new_frame = zigbee_packet_encrypt(TRANSPOT_KEY, new_parsed_frame, bytes(new_payload), extended_source_bytes)
-        # encrypted_frame = new_frame.build()
-        encrypted_frame = encrypted_toggle(0xae03, 0x0000, 0x3ef6, 0x00212effff03fe6a, TRANSPOT_KEY, fc).build()
-        send_packet(ser, encrypted_frame[:-2])
-        fc+=1
+        frame_counter += 1
+        seq_num = (seq_num + 1) % 256
+        nwk_seq_num = (nwk_seq_num + 1) % 256
+        aps_counter = (aps_counter + 1) % 256
+        zcl_seq_num = (zcl_seq_num + 1) % 256
 
 if __name__ == "__main__":
     main()
--- zigbear/packets.py
+++ zigbear/packets.py
@@ -1,6 +1,7 @@
+import struct
 from scapy.config import conf
 from scapy.layers.dot15d4 import Dot15d4Data, Dot15d4FCS
-from zigbear.patch.zigbee import ZigbeeAppDataPayload, ZigbeeAppDataPayloadStub, ZigbeeNWK, ZigbeeSecurityHeader, ZigbeeClusterLibrary
+from zigbear.patch.zigbee import ZigbeeAppDataPayload, ZigbeeAppDataPayloadStub, ZigbeeNWK, ZigbeeNWKStub, ZigbeeSecurityHeader, ZigbeeClusterLibrary, ZigbeeZLLCommissioningCluster, ZLLIdentifyRequest
 
 from zigbear.crypto import zigbee_packet_decrypt, zigbee_packet_encrypt
 from zigbear.utils import pan, address, extended_address, extended_address_bytes, get_extended_source
@@ -10,16 +11,16 @@ conf.dot15d4_protocol = "zigbee"
 
 def dot15d4_data_stub(seq_num, panid, source, destination):
     dot15d4 = Dot15d4FCS()
-    dot15d4.fcf_frametype=1
-    dot15d4.fcf_destaddrmode=2
-    dot15d4.fcf_srcaddrmode=2
-    dot15d4.fcf_panidcompress=1
-    dot15d4.fcf_ackreq=1
-    dot15d4.seqnum=seq_num
+    dot15d4.fcf_frametype = 1
+    dot15d4.fcf_destaddrmode = 2
+    dot15d4.fcf_srcaddrmode = 2
+    dot15d4.fcf_panidcompress = 1
+    dot15d4.fcf_ackreq = 1
+    dot15d4.seqnum = seq_num
 
     dot15d4_data = Dot15d4Data()
-    dot15d4_data.dest_panid=panid
-    dot15d4_data.dest_addr=destination
+    dot15d4_data.dest_panid = panid
+    dot15d4_data.dest_addr = destination
     dot15d4_data.src_addr = source
 
     return dot15d4 / dot15d4_data
@@ -27,24 +28,24 @@ def dot15d4_data_stub(seq_num, panid, source, destination):
 
 def nwk_stub(source, destination, seqnum, extended_source=None):
     stub = ZigbeeNWK()
-    stub.frametype=0
-    stub.discover_route=1
-    stub.proto_version=2
-    stub.flags=2
-    stub.destination=destination
-    stub.source=source
-    stub.radius=30
-    stub.seqnum=seqnum
+    stub.frametype = 0
+    stub.discover_route = 1
+    stub.proto_version = 2
+    stub.flags = 2
+    stub.destination = destination
+    stub.source = source
+    stub.radius = 30
+    stub.seqnum = seqnum
     if extended_source is not None:
-        stub.extended_source=extended_source
+        stub.extended_source = extended_source
     return stub
 
 
 def security_header_stub(extended_source, frame_counter):
     stub = ZigbeeSecurityHeader()
-    stub.key_type=1
-    stub.fc=frame_counter
-    stub.source=extended_source
+    stub.key_type = 1
+    stub.fc = frame_counter
+    stub.source = extended_source
     return stub
 
 
@@ -53,61 +54,27 @@ def encrypted_toggle(panid, source, destination, extended_source, key, frame_cou
     source = address(source)
     destination = address(destination)
     extended_source = extended_address(extended_source)
-
     extended_source_bytes = extended_address_bytes(extended_source)
-
-    aps_payload = bytearray(b"\x0c\x02\x00\x06\x00\x04\x01\x01\xf4\x11\x9c\x02")
-    # bump Zigbee APS counter
-    #aps_payload[7] = aps_counter
-    # bump ZCL sequence number
-    #aps_payload[9] = zcl_seq_num
-    # set command to toggle
-    #aps_payload[10] = 2
 
     dot15d4_data = dot15d4_data_stub(seq_num, panid, source, destination)
     nwk = nwk_stub(source, destination, nwk_seq_num)
     security_header = security_header_stub(extended_source, frame_counter)
+
+    aps = ZigbeeAppDataPayload()
+    aps.frame_control = 0
+    aps.delivery_mode = 2
+    aps.dst_endpoint = 255
+    aps.cluster = 0x0006  # on/off cluster
+    aps.profile = "HA_Home_Automation"
+    aps.src_endpoint = 1
+
+    zcl = ZigbeeClusterLibrary()
+    zcl.zcl_frametype = 1  # cluster specific
+    zcl.command_identifier = 0x02  # toggle
+    zcl.disable_default_response = 1
+    zcl.transaction_sequence = zcl_seq_num
+
     unencrypted_frame_part = dot15d4_data / nwk / security_header
+    payload = aps / zcl
 
-    return zigbee_packet_encrypt(key, unencrypted_frame_part, bytes(aps_payload), extended_source_bytes)
-
-
-def parse_frame(frame):
-    pkt = Dot15d4FCS(frame)
-    pkt.nwk_seclevel = 5
-    pkt.data += pkt.mic
-    pkt.mic = pkt.data[-4:]
-    pkt.data = pkt.data[:-4]
-    return pkt
-
-
-def bump_counters(parsed_frame, decrypted_payload, min_fc=0):
-    aps_packet = ZigbeeAppDataPayloadStub(decrypted_payload)
-    aps_load = bytearray(aps_packet.load)
-
-    parsed_frame.data = ""
-    parsed_frame.mic = ""
-
-    # bump 802.15.4 sequene number
-    parsed_frame[Dot15d4FCS].seqnum = (parsed_frame[Dot15d4FCS].seqnum + 1) % 255
-    # bump Zigbee NWK sequene number
-    parsed_frame[ZigbeeNWK].seqnum = (parsed_frame[ZigbeeNWK].seqnum + 1) % 255
-    # bump Zigbee Security header frame counter
-    parsed_frame[ZigbeeSecurityHeader].fc = max(min_fc, parsed_frame[ZigbeeSecurityHeader].fc) + 1
-    # bump Zigbee APS counter
-    aps_load[1] = (aps_load[1] + 1) % 255
-    # bump ZCL sequence number
-    aps_load[3] = (aps_load[3] + 1) % 255
-    # set command to toggle
-    aps_load[4] = 2
-
-    print(f'802.15.4 sequence number: {parsed_frame[Dot15d4FCS].seqnum}')
-    print(f'ZigbeeNWK sequence number: {parsed_frame[ZigbeeNWK].seqnum}')
-    print(f'Zigbee Security header frame counter: {parsed_frame[ZigbeeSecurityHeader].fc}')
-    print(f'Zigbee APS counter: {aps_load[1]}')
-    print(f'ZCL sequence number: {aps_load[3]}')
-
-    aps_packet.load = aps_load
-    new_payload = aps_packet.build()
-
-    return parsed_frame, new_payload
+    return zigbee_packet_encrypt(key, unencrypted_frame_part, bytes(payload), extended_source_bytes)
